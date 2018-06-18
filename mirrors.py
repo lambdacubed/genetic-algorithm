8025128c7c7d53e8146a17c3898d7bb35a0da2a8
markmath <markmath@umich.edu>
2018-06-18 12:29:02 -0400

    merge lab_computer

--- mirrors.py
+++ mirrors.py
@@ -19,6 +19,18 @@ def initialize_mirror(which_mirror):
     else:
         print("You didn't enter a correct mirror.")
 
+<<<<<<< HEAD
+=======
+    Attributes
+    ----------
+    dm_actuator_neighbors: deformable mirror actuator neighbors, numpy array
+        The array contains all of the pairs of actuators which neighbor each other (including diagonal)
+    """
+    def __init__(self):
+        self.max_difference =  20 # maximum difference in voltage between neighboring actuators
+        self.max_voltage = 100 # maximumm voltage an acuator can have
+        self.min_voltage = 0
+>>>>>>> lab_computer
 
 
 class deformable_mirror(object):
